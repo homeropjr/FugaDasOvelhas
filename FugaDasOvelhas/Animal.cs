9afc7b7ba48b170445e82f6d088b86b24f2ed704
homeropjr <homeropjr@gmail.com>
2019-03-23 22:44:14 -0300

    Criado o spawn dos objetos.

--- FugaDasOvelhas/Animal.cs
+++ FugaDasOvelhas/Animal.cs
@@ -1,28 +1,18 @@
 ﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FugaDasOvelhas
 {
     public class Animal
     {
-
         public Texture2D texture { get; set; }
 
         public Vector2 position { get; set; }
 
-        private SoundEffect soundEffect;
-
-        public Animal(Game game, Vector2 position, Texture2D texture, string content)
+        public Animal(Game game, Vector2 position, string textureName)
         {
             this.position = position;
-            this.texture = game.Content.Load<Texture2D>(texture.Name);
-            this.soundEffect = game.Content.Load<SoundEffect>(content);            
+            this.texture = game.Content.Load<Texture2D>(textureName);
         }
 
         public void Draw(SpriteBatch spriteBatch)
--- FugaDasOvelhas/Game1.cs
+++ FugaDasOvelhas/Game1.cs
@@ -3,37 +3,21 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 
 namespace FugaDasOvelhas
 {
     public class Game1 : Game
     {
-
-        public enum Animals
-        {
-            Dog,
-            Cow,
-            Horse,
-            Pig,
-            Cat,
-            Sheep
-        }
-
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
         private Player player;
-        private Sheep sheep;
         private Random random;
         private Texture2D background;
-        private Dog dog;
-        private Cow cow;
-        private Horse horse;
-        private Pig pig;
-        private Cat cat;
 
         private IList<Animal> animalsFarm;
+
+        private float elapsedTime = 3f;
 
         public Game1()
         {
@@ -51,18 +35,11 @@ namespace FugaDasOvelhas
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
+
             random = new Random();
             animalsFarm = new List<Animal>();
-
             background = Content.Load<Texture2D>("background");
-
             player = new Player(this, new Vector2(340f, 70f));
-            dog = new Dog(this, new Vector2(10f, 360f));
-            cow = new Cow(this, new Vector2(30f, 360f));
-            horse = new Horse(this, new Vector2(70f, 360f));
-            pig = new Pig(this, new Vector2(130f, 360f));
-            cat = new Cat(this, new Vector2(230f, 360f));
-
         }
 
         protected override void UnloadContent()
@@ -74,6 +51,7 @@ namespace FugaDasOvelhas
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+
             KeyboardState key = Keyboard.GetState();
             if (key.IsKeyDown(Keys.Left))
                 player.MoveParaEsquerda();
@@ -91,7 +69,9 @@ namespace FugaDasOvelhas
 
             spriteBatch.Begin();
 
-            spriteBatch.Draw(background, new Vector2(0, 0), Color.White);            
+            spriteBatch.Draw(background, new Vector2(0, 0), Color.White);
+
+            player.Draw(spriteBatch);
 
             foreach (Animal animal in animalsFarm)
                 animal.Draw(spriteBatch);
@@ -104,39 +84,37 @@ namespace FugaDasOvelhas
         public void Spawn()
         {
             int i = random.Next(0, 5);
-            Vector2 position = new Vector2(random.Next(0, 800), 340f);
+            Vector2 position = new Vector2(random.Next(0, 800), 360f);
 
-            switch ((Animals)i)
+            switch (i)
             {
-                case Animals.Dog:
-                    Texture2D texture = new Texture2D();
-                    texture.Name = "Dog";
-                    animalsFarm.Add(new Animal(this, position, texture, "");
+                case 1:
+                    animalsFarm.Add(new Animal(this, position, "cat"));
                     break;
 
-                case Animals.Cow:
-                    animalsFarm.Add(new Cow(this, positon));
-                    break;
-                    
-                    case Animals.Horse:
-                    animalsFarm.Add(new Horse(this, positon));
+                case 2:
+                    animalsFarm.Add(new Animal(this, position, "dog"));
                     break;
 
-                case Animals.Pig:
-                    animalsFarm.Add(new Pig(this, positon));
+                case 3:
+                    animalsFarm.Add(new Animal(this, position, "cow"));
                     break;
 
-                case Animals.Cat:
-                    animalsFarm.Add(new Cat(this, positon));
+                case 4:
+                    animalsFarm.Add(new Animal(this, position, "horse"));
                     break;
 
-                case Animals.Sheep:
-                    animalsFarm.Add(new Sheep(this, positon));
+                case 5:
+                    animalsFarm.Add(new Animal(this, position, "pig"));
                     break;
-                    
+
+                case 6:
+                    animalsFarm.Add(new Animal(this, position, "sheep"));
+                    break;
                 default:
                     break;
             }
+
         }
     }
 }
